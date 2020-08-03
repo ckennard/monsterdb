245269d3af6f11bb4739e08189444da3e0eb772b
Chris Kennard <chriskennard123@gmail.com>
2020-08-03 00:08:40 -0500

    Added sorting and POST

--- app.py
+++ app.py
@@ -2,10 +2,14 @@
 # Previous imports remain...
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask import Flask,request, render_template
+from flask import Flask, request, render_template
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:ThanksP0stgres!@localhost:5432/bestiary"
+app.config['SECRET_KEY'] = "tylerskelton"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -25,20 +29,34 @@ class monstersModel(db.Model):
     def __repr__(self):
         return f"<Monster {self.Name}>"
 
-@app.route('/monsters', methods=['GET'])
+class SortForm(FlaskForm):
+    sort = SelectField('sort', 
+        validators=[DataRequired()], 
+        choices=[('Name', 'Name'),('CR', 'CR'), ('XP', 'XP')]
+    )
+
+@app.route('/monsters', methods=['GET','POST'])
 def handle_monsters():
     if request.method == 'GET':
         monsters = monstersModel.query.all()
-        results = [
-            {
-                "name": monster.Name,
-                "cr": monster.CR,
-                "xp": monster.XP
-            } for monster in monsters]
+        form = SortForm()
+    else:
+        form = SortForm(request.form)
+        if form.validate():
+            monsters = monstersModel.query.order_by(form.sort.data)
+            print ("successful form all good")
+        else:
+            print(form.errors)
+            print("bullshit form i hate it")
 
-        #return {"count": len(results), "monsters": results}
-        return render_template("monsters.html",monsters=results)
+    results = [{
+        "name": monster.Name,
+        "cr": monster.CR,
+        "xp": monster.XP
+    } for monster in monsters]
+
+    return render_template("monsters.html",monsters=results,form=form)
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
