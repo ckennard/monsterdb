3e3a190a6d7f812e264510d8984e2f34dc219d09
Chris Kennard <chriskennard123@gmail.com>
2020-08-24 16:26:47 -0500

    Set up nice formatting and added Alignment, Size, and Environment

--- app.py
+++ app.py
@@ -19,12 +19,18 @@ class monstersModel(db.Model):
     CR = db.Column(db.Integer())
     Type = db.Column(db.String())
     Source = db.Column(db.String())
+    Alignment = db.Column(db.String())
+    Size = db.Column(db.String())
+    Environment = db.Column(db.String()) 
 
-    def __init__(self, Name, CR, Type, Source):
+    def __init__(self, Name, CR, Type, Source, Alignment, Size, Environment):
         self.Name = Name
         self.CR = CR
         self.Type = Type
         self.Source = Source
+        self.Alignment = Alignment
+        self.Size = Size
+        self.Environment = Environment
 
     def __repr__(self):
         return f"<{self.Name}>"
@@ -34,14 +40,13 @@ def _get_form(data=None):
     form.cr.choices = [('All CRs', 'All CRs')]+[(r.CR,r.CR) for r in monstersModel.query.distinct('CR').order_by("CR")]
     form.Type.choices = [('All Types', 'All Types')]+[(r.Type,r.Type.capitalize()) for r in monstersModel.query.distinct('Type').order_by("Type")]
     form.Source.choices = [('All Sources', 'All Sources')]+[(r.Source,r.Source) for r in monstersModel.query.distinct('Source').order_by("Source")]
+    form.Alignment.choices = [('All Alignments', 'All Alignments')]+[(r.Alignment,r.Alignment) for r in monstersModel.query.distinct('Alignment').order_by("Alignment")]
+    form.Size.choices = [('All Sizes', 'All Sizes')]+[(r.Size,r.Size) for r in monstersModel.query.distinct('Size').order_by("Size")]
+    form.Environment.choices = [('All Environments', 'All Environments')]+[(r.Environment,r.Environment) for r in monstersModel.query.distinct('Environment').order_by("Environment")]
     return form
     
 class FilterForm(FlaskForm):
     print("filterform")
-    sort = SelectField('Sort by...', 
-        validators=[DataRequired()], 
-        choices=[('Name', 'Name'),('CR', 'CR'), ('Type', 'Type'),('Source', 'Source')]
-    )
     cr = SelectMultipleField('CR',
         validators=[Optional()],
         choices=[]
@@ -51,6 +56,18 @@ class FilterForm(FlaskForm):
         choices=[]
     )
     Source = SelectMultipleField('Source',
+        validators=[Optional()],
+        choices=[]
+    )
+    Alignment = SelectMultipleField('Alignment',
+        validators=[Optional()],
+        choices=[]
+    )
+    Size = SelectMultipleField('Size',
+        validators=[Optional()],
+        choices=[]
+    )
+    Environment = SelectMultipleField('Environment',
         validators=[Optional()],
         choices=[]
     )
@@ -64,7 +81,6 @@ def handle_monsters():
     else:
         form = _get_form(request.form)
         if form.validate():
-            monsters = monsters.order_by(form.sort.data)
             if form.cr.data:
                 if not 'All CRs' in form.cr.data: 
                     print ("CR filter detected")
@@ -83,6 +99,24 @@ def handle_monsters():
                     monsters = monsters.filter(monstersModel.Source.in_(form.Source.data))
                 else:
                     print("All Sources selected")
+            if form.Alignment.data:
+                if not 'All Alignments' in form.Alignment.data: 
+                    print ("Alignment filter detected")
+                    monsters = monsters.filter(monstersModel.Alignment.in_(form.Alignment.data))
+                else:
+                    print("All Alignments selected")
+            if form.Size.data:
+                if not 'All Sizes' in form.Size.data: 
+                    print ("Size filter detected")
+                    monsters = monsters.filter(monstersModel.Size.in_(form.Size.data))
+                else:
+                    print("All Sizes selected")
+            if form.Environment.data:
+                if not 'All Environments' in form.Environment.data: 
+                    print ("Environment filter detected")
+                    monsters = monsters.filter(monstersModel.Environment.in_(form.Environment.data))
+                else:
+                    print("All Environments selected")
             print ("successful form all good")
         else:
             print(form.errors)
@@ -93,7 +127,10 @@ def handle_monsters():
         "name": monster.Name,
         "cr": monster.CR,
         "Type": monster.Type,
-        "Source": monster.Source
+        "Source": monster.Source,
+        "Alignment": monster.Alignment,
+        "Size": monster.Size,
+        "Environment": monster.Environment
     } for monster in monsters]
 
     return render_template("monsters.html",monsters=results,form=form)
